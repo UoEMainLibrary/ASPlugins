5b37b166bb8b350bcb35491bcf31b6fa11692971
Scott Renton <scott.renton@ed.ac.uk>
2018-09-05 12:32:34 +0100

    LIB001-2115 add to git

---
+++ plugins/local/public/plugin_init.rb
@@ -0,0 +1,14 @@
+Rails.application.config.after_initialize do
+  class PdfController <  ApplicationController
+    skip_before_action :verify_authenticity_token
+  end
+end
+
+Rails.application.config.after_initialize do
+  class RequestsController <  ApplicationController
+    skip_before_action :verify_authenticity_token
+  end
+end
+
+
+
